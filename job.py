64e48aed401b4fbaed15a0964e546ec111e14e89
ajay <ajaykumar@digitalocean.com>
2025-07-24 13:40:13 +0000

    .

--- job.py
+++ job.py
@@ -1,8 +1,8 @@
 import datetime
 
 def main():
-    print("Hello from DigitalOcean App Platform Job!")
-    print("Current time:", datetime.datetime.utcnow())
+    print("Hello from DigitalOcean scheduled job!")
+    print("Current UTC time:", datetime.datetime.utcnow())
 
 if __name__ == "__main__":
     main()
